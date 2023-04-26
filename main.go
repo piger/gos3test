aa141ffbdd1b5594cd88bf8ba288eb2ea06c1e54
Daniel Kertesz <daniel@spatof.org>
2023-04-26 20:44:44 +0100

    use functions to initialise the clients

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
@@ -16,28 +15,38 @@ const (
 	filename = "foo.bar"
 )
 
-func endpointResolver(service, region string, options ...any) (aws.Endpoint, error) {
-	return aws.Endpoint{
-		PartitionID:   "aws",
-		URL:           "http://localhost:4566",
-		SigningRegion: "us-east-1",
-	}, nil
+// testConfig creates an AWS configuration using test credentials and an endpoint resolver
+// that points to localstack on localhost.
+func testConfig() aws.Config {
+	endpointResolver := func(service, region string, options ...any) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			PartitionID:   "aws",
+			URL:           "http://localhost:4566",
+			SigningRegion: "us-east-1",
+		}, nil
+	}
+
+	return aws.Config{
+		Region:                      "us-east-1",
+		EndpointResolverWithOptions: aws.EndpointResolverWithOptionsFunc(endpointResolver),
+		Credentials:                 credentials.NewStaticCredentialsProvider("AKID", "SECRET_KEY", "TOKEN"),
+	}
+}
+
+// testS3Client creates an S3 client from the given AWS configuration, and configure it
+// to use "Path Style" (i.e. http://s3.amazonaws.com/BUCKET-NAME/key); this is needed to use S3
+// with localstack without having to create custom DNS entries for each bucket.
+func testS3Client(cfg aws.Config) *s3.Client {
+	return s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = true
+	})
 }
 
 func run() error {
 	ctx := context.Background()
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion("us-east-1"),
-		config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(endpointResolver)),
-		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("AKID", "SECRET_KEY", "TOKEN")),
-	)
-	if err != nil {
-		return err
-	}
 
-	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
-		o.UsePathStyle = true
-	})
+	cfg := testConfig()
+	client := testS3Client(cfg)
 
 	// create the bucket
 	if _, err := client.CreateBucket(ctx, &s3.CreateBucketInput{
